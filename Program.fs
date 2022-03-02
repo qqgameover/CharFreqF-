18ade5c62f6d420e554b1ce7f5f67bea358431a9
Kasper L <kasper35@live.no>
2022-03-02 23:29:03 +0100

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let letterFreq (text : string) =
+    text.ToUpper().ToCharArray()
+    |> Array.filter (fun x -> (['A'..'Z']|>Set.ofList).Contains(x))
+    |> Seq.countBy (fun x -> x)
+    |> Seq.sortByDescending (fun (_, x) -> x)
+
+[<EntryPoint>]
+let main argv =
+    let res = letterFreq "Hello this is pretty neat"
+    for (letter, freq) in res do
+        printfn "Letter: %c frequency: %d" letter freq
+    0
